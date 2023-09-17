424c6a127dfdfe228f0fad3dceea641cddcf3123
BalaSzil <balazs.gergely.szilagyi@gmail.com>
2023-09-17 02:27:04 +0200

    Cleaning up

--- BattleshipsV0.0.py
+++ BattleshipsV0.0.py
@@ -1,37 +1,37 @@
 # Functions
 import string
 import random
-def rowing(row_name):
-    row_name = " ".join(row_name)
-    return row_name
 def exes(name_of_list):
+    """Gives you ten X-es for the row"""
     name_of_list.extend(list("X"*10))
     return name_of_list
 def vector(name_of_master_list):
+    """"Returns random number within the ML lenght"""
     pos_value = random.randrange((len(name_of_master_list)))
     return pos_value
 def vector_sans_l(name_of_master_list, ship_length):
+    """Returns random number within ML minus ship lenght"""
     pos_value = random.randrange((len(name_of_master_list)) - ship_length)
     return pos_value
-def placement(ship, ship_length):
-    while ship > 0:
-        placement_decision = random.randrange(2)
-        if placement_decision == 0:
+def placement(ship_no, ship_length):
+    """Places given type of ship, returns nothing"""
+    while ship_no > 0:
+        is_vertical = random.randrange(2) == 0
+        if is_vertical:
             x = vector(Master_list)
-            y = vector_sans_l(Master_list[x], ship_length)
-            for i in range(ship_length):
-                if all(Master_list[y + i][x] == "X" for i in range(ship_length)):
-                    for i in range(ship_length):
-                        Master_list[y + i][x] = "S"
-                    ship -= 1
-        elif placement_decision == 1:
+            y = vector_sans_l(Master_list, ship_length)
+            if all(Master_list[y + i][x] == "X" for i in range(ship_length)):
+                for i in range(ship_length):
+                    Master_list[y + i][x] = "S"
+                ship_no -= 1
+        else:
             x = vector_sans_l(Master_list, ship_length)
             y = vector(Master_list)
-            for i in range(ship_length):
-                if all(Master_list[y][x + i] == "X" for i in range(ship_length)):
-                    for i in range(ship_length):
-                        Master_list[y][x + i] = "S"
-                    ship -= 1
+            if all(Master_list[y][x + i] == "X" for i in range(ship_length)):
+                for i in range(ship_length):
+                    Master_list[y][x + i] = "S"
+                ship_no -= 1
+    
     
 # Setup
 print("Welcome! Let's play a round, I'll prepare my battleships!")
@@ -45,11 +45,12 @@ Battleship_l = 4
 Cruiser_l = 3
 Submarine_l = 3
 Destroyer_l = 2
-Force = (Carriers * Carrier_l +
-         Battleships * Battleship_l +
-         Cruisers * Cruiser_l +
-         Submarines * Submarine_l +
-         Destroyers * Destroyer_l)
+force = (Carriers * Carrier_l
+         + Battleships * Battleship_l
+         + Cruisers * Cruiser_l
+         + Submarines * Submarine_l
+         + Destroyers * Destroyer_l)
+torpedos = 10
 A = []
 B = []
 C = []
@@ -61,10 +62,6 @@ H = []
 I = []
 J = []
 Master_list = [exes(A), exes(B), exes(C), exes(D), exes(E), exes(F), exes(G), exes(H), exes(I), exes(J)]
-# letter = C
-# position = ord(letter) - ord(A)
-# print(position)
-# print(ord("A"))
 
 placement(Carriers, Carrier_l)
 placement(Battleships, Battleship_l)
@@ -72,45 +69,60 @@ placement(Cruisers, Cruiser_l)
 placement(Submarines, Submarine_l)
 placement(Destroyers, Destroyer_l)
 
-count = 0
-for sublist in Master_list:
-    for element in sublist:
-        if element == "S":
-            count += 1
-print(count)
-print(Master_list)
+# count = 0
+# for sublist in Master_list:
+#     for element in sublist:
+#         if element == "S":
+#             count += 1
+# print(count)
+# print(Master_list)
 
 
+# Playerboard
 print(f"I have {Carriers} carriers, {Battleships} battleships, {Cruisers} cruisers, {Submarines} submarines, and {Destroyers} destroyers.")
 print(f"Carriers are {Carrier_l}, the battleships {Battleship_l}, the cruisers {Cruiser_l}, the submarines {Submarine_l}, and the destroyers {Destroyer_l} large.")
-print("None are placed diagonally.")
-print(f"You need a total of {Force} hits to win!")
-# rows = (string.ascii_uppercase[0:10])
-# columns = (range(1,11))
+print("Ships can be placed next to one another, but none are placed diagonally.")
+print(f"You need a total of {force} hits to win!")
+rows = (string.ascii_uppercase[0:10])
+columns = (range(1,11))
 
-# Playing field
-# You could make 10 lists (1 for each row), have the input refer to list number/list element
-# Replace with "[ ]" for miss, "H" for hit.
+helper = 0
+for i in columns:
+    print(i, end = " ")
+print()
+for Y in rows:
+    for X in range (10):
+        print("X", end = " ")
+    print(rows[helper : helper + 1])
+    helper += 1
+    
+    
+# Gameplay
+while torpedos > 0 and force > 0:
+    guess = input(f"You can only miss {torpedos} times! Use the NumberLetter (e.g. A1) format and make a guess! ")
+    guess = guess.upper()
+    if len(guess) != 2:
+        print("Please use the NumberLetter format. One letter, one number. No more, no less.")
+    elif ord(guess[0]) < 65 or ord(guess[0]) > 90:
+        print("Please use the NumberLetter format. One letter, one number. No more, no less.")
+    elif ord(guess[1]) < 48 or ord(guess[1]) > 57:
+        print("Please use the NumberLetter format. One letter, one number. No more, no less.")
+    else:
+        letter = guess[0]
+        number = int(guess[1])
+        hor_position = ord(letter) - (ord("A"))
+        ver_position = (number - 1)
+        guess_pos = Master_list[hor_position][ver_position]
+        if guess_pos == "S":
+            guess_pos = "H"
+            force -= 1
+            print(f"That's a hit!!! The enemy is left at {force} strength!")
+            print(Master_list)
+        else:
+            guess_pos = "O"
+            torpedos -= 1
+            print(f"That's a miss! You only try {torpedos} more times!")
+            print(Master_list)
 
-# Placement
-# Use list.remove() and list.insert() to change elements from above lists
-# Can base orientation of placed ships on randomization! Range 0-9, hor if even, vert if odd.
-# Carrier_plac = Carriers * Carrier_l
-# Battleship_plac = Battleships * Battleship_l
-# Cruiser_plac = Cruisers * Cruiser_l
-# Submarine_plac = Submarines * Submarine_l
-# Destroyer_plac = Destroyers * Destroyer_l
-# helper = 0
 
-# helper = 0
-# for i in columns:
-#     print(i, end = " ")
-# print()
-# for Y in rows:
-#     for X in range (10):
-#         print("X", end = " ")
-#     print(rows[helper:helper+1])
-#     helper +=1
-# guess = input("Use the NumberLetter (e.g. A1) format and make a guess! ")
-# for victory, hits == Force vagy while Force > 0
-# Eliminate invalid guesses (l or s than 2 char, 2 letters, 2 numbers)
+# Eliminate invalid guesses (l or s then 2 char, 2 letters, 2 numbers). Based on ordenance number ranges maybe?
--- HangmanV0.0.py
+++ HangmanV0.0.py
@@ -1,19 +1,22 @@
-words = ["tequila", "sunset", "disco", "dancer", "column", "mariner", "lighthouse", "insanity", "janitor", "rat", "cheese", "peach", "laxative", "archer", "tactical", "turtleneck", "oedipus", "literary", "difficulty", "msspelled", "xenophon"]
-welcome = ["Hey there! Welcome!", "How you doin'?", "What are you looking at, weirdo? Oh yeah, the game...", "So glad to see you here!"]
-fail_insults = ["Wroooong! You suck!", "Wow, just, wow...", "Andrew, is that you?", "Hint: the word is in English. Do you speak it??? DOES HE LOOK LIKE A BI... sorry, sorry..."]
-success_insults = ["You got one! Damn you!!!", "Even a broken clock...", "Well, color me impressed...", "OK, OK, but don't get cocky on me now."]
-final_insults = ["Well, it seems you failed. Should I have made a tutorial or something?", "Oooooh, I'm so sorry, I didn't know you were illiterate!", "Wow... words fail me... as they have obviously failed you.", "I mean... should I bother insulting you, can you even read this?", "We're done here, STOP STARING AT ME!!!"]
-guess_prompts = ["Make your guess, foolish mortal: ", "Come on, try something: ", "Gimme a letter: ", "Just press one of the buttons in front of you, meatbag: ", "Please insert letter: "]
-banned = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "'", "+", "-", "*", "!", "%", "/", "=", "(", ")"]
 import random
 def randomize(factor):
     return factor[random.randrange(len(factor))]
+
+words = ["tequila", "sunset", "disco", "dancer", "column", "mariner", "lighthouse", "insanity", "janitor", "rat", "cheese", "peach", "laxative", "archer", "tactical", "turtleneck", "oedipus", "literary", "difficulty", "msspelled", "xenophon"]
+welcome = ["Hey there! Welcome!", "How you doin'?", "What are you looking at, weirdo? Oh yeah, the game...", "So glad to see you here!"]
+fail_insults = ["Wroooong! You suck!", "Wow, just, wow...", "Andrew, is that you?", "Hint: the word is in English. Do you speak it??? DOES HE LOOK LIKE A BI... sorry, sorry..."]
+success_insults = ["GG", "You got one! Damn you!!!", "Even a broken clock...", "Well, color me impressed...", "OK, OK, but don't get cocky on me now."]
+final_insults = ["Well, it seems you failed. Should I have made a tutorial or something?", "Oooooh, I'm so sorry, I didn't know you were illiterate!", "Wow... words fail me... as they have obviously failed you.", "I mean... should I bother insulting you, can you even read this?", "We're done here, STOP STARING AT ME!!!"]
+guess_prompts = ["Make your guess, foolish mortal: ", "Come on, try something: ", "Gimme a letter: ", "Just press one of the buttons in front of you, meatbag: ", "Please insert letter: "]
+banned = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "'", "+", "-", "*", "!", "%", "/", "=", "(", ")"]
+
 prob = randomize(words)
-representation = "X"*len (prob)
+representation = "X"*len(prob)
 print(randomize(welcome))
 print(representation)
+
 tries = 8
-while tries>0:
+while tries > 0:
     if representation == prob:
         print("I guess we're done here...")
         break
@@ -31,6 +34,9 @@ while tries>0:
     elif guess not in prob:
         tries -= 1
         print(randomize(fail_insults), f"You have {tries} guesses remaining!")
+    elif guess in representation:
+        tries -= 1
+        print(f"Come on, mate, you've already tried that! There goes one of your guesses, you've got {tries} remaining...")
     elif guess in prob:
         for i in range(len(prob)):
             if prob[i] == guess:
