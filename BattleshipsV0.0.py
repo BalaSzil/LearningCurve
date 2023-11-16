c6974483bdac63591f53b35bf30d68c45aa77627
BalaSzil <balazs.gergely.szilagyi@gmail.com>
2023-11-16 20:39:58 +0100

    Updated

--- BattleshipsV0.0.py
+++ BattleshipsV0.0.py
@@ -1,59 +1,68 @@
-# Functions
 import string
 import copy
 from random import randrange
+from colorama import init, Fore, Style
 
+init()
 
-def Fill_Board_Recurring_Characters(board, character, length):
+def fill_board_with_recurring_characters(board, character, length):
     for i in range(length):
         board.append(list(f"{character}"*length))
     return board
 
 
-def Generate_Coordinate(board):
-    cord_value = randrange((len(board)))
+def generate_coordinate(board):
+    cord_value = randrange(len(board))
     return cord_value
 
 
-def Generate_Coordinate_Ship_Start(board, ship_length):
-    cord_value = randrange((len(board)) - ship_length)
+def generate_coordinate_ship_start(board, ship_length):
+    cord_value = randrange(len(board) - ship_length)
     return cord_value
 
 
-def Place_Ships(ship_no, ship_length):
+def place_ships(ship_no, ship_length):
     while ship_no > 0:
         is_vertical = randrange(2) == 0
         if is_vertical:
-            x = Generate_Coordinate(hidden_board)
-            y = Generate_Coordinate_Ship_Start(hidden_board, ship_length)
+            x = generate_coordinate(hidden_board)
+            y = generate_coordinate_ship_start(hidden_board, ship_length)
             if all(hidden_board[y + i][x] == "X" for i in range(ship_length)):
                 for i in range(ship_length):
                     hidden_board[y + i][x] = "S"
                 ship_no -= 1
         else:
-            x = Generate_Coordinate_Ship_Start(hidden_board, ship_length)
-            y = Generate_Coordinate(hidden_board)
+            x = generate_coordinate_ship_start(hidden_board, ship_length)
+            y = generate_coordinate(hidden_board)
             if all(hidden_board[y][x + i] == "X" for i in range(ship_length)):
                 for i in range(ship_length):
                     hidden_board[y][x + i] = "S"
                 ship_no -= 1
 
-
-def Print_Board(visible_board):
-    helper = 0
-    for Y in range(1,(1 + len(visible_board))):
-        print(" ", Y, end = "  ")
+def print_board(board):
+    for i in range(1, (1 + len(board))):
+        print(i, end = " ")
     print()
-    for X in range(len(visible_board[0])):
-        print(visible_board[helper], end = " ")
-        print(string.ascii_uppercase[helper])
-        helper += 1
-        
-        
-# Setup
+    for j in range(len(board[0])):
+        for char in board[j]:
+            if char == "H":
+                print(Fore.RED + char + Style.RESET_ALL, end = " ")
+            elif char == "O":
+                print(Fore.BLUE + char + Style.RESET_ALL, end = " ")
+            else:
+                print(char, end = " ")
+        print(string.ascii_uppercase[j])
+
+def intro():
+    print(f"I have {Carriers} carrier(s), {Battleships} battleships, {Cruisers} cruisers, {Submarines} submarines, and {Destroyers} destroyers.")
+    print(f"A carrier is {Carrier_l}, a battleship {Battleship_l}, a cruiser {Cruiser_l}, a submarine {Submarine_l}, and a destroyer {Destroyer_l} cells long.")
+    print("Ships can be placed next to one another, but none are placed diagonally.")
+    print(f"You need a total of {force} hits to win!")
+
+       
 print("Welcome! Let's play a round, I'll prepare my battleships!")
 hidden_board = []
-Fill_Board_Recurring_Characters(hidden_board, "X", 10)
+fill_board_with_recurring_characters(hidden_board, "X", 10)
 visible_board = copy.deepcopy(hidden_board)
 Carriers = 1
 Battleships = 2
@@ -72,55 +81,54 @@ force = (Carriers * Carrier_l
          + Destroyers * Destroyer_l)
 torpedos = 30
 
-Place_Ships(Carriers, Carrier_l)
-Place_Ships(Battleships, Battleship_l)
-Place_Ships(Cruisers, Cruiser_l)
-Place_Ships(Submarines, Submarine_l)
-Place_Ships(Destroyers, Destroyer_l)
+place_ships(Carriers, Carrier_l)
+place_ships(Battleships, Battleship_l)
+place_ships(Cruisers, Cruiser_l)
+place_ships(Submarines, Submarine_l)
+place_ships(Destroyers, Destroyer_l)
+
+intro()
+print_board(visible_board)
 
 
-# Playerboard
-print(f"I have {Carriers} carrier(s), {Battleships} battleships, {Cruisers} cruisers, {Submarines} submarines, and {Destroyers} destroyers.")
-print(f"A carrier is {Carrier_l}, a battleship {Battleship_l}, a cruiser {Cruiser_l}, a submarine {Submarine_l}, and a destroyer {Destroyer_l} cells long.")
-print("Ships can be placed next to one another, but none are placed diagonally.")
-print(f"You need a total of {force} hits to win!")
-
-
-Print_Board(visible_board)
-
-
-# Gameplay
 while torpedos > 0 and force > 0:
+    
     guess = input(f"You can only miss {torpedos} times! Use the NumberLetter (e.g. A1) format and make a guess! ")
     guess = guess.upper()
-    Is_Guess_Not_Properly_Formatted = guess == "" or not guess[0].isalpha() or not guess[1:].isnumeric()
-    if Is_Guess_Not_Properly_Formatted:
+    
+    is_guess_properly_formatted = guess and guess[0].isalpha() and guess[1:].isnumeric()
+    if not is_guess_properly_formatted:
         print("Please use the NumberLetter format. One letter, one number. No more, no less.")
+        continue
+    
+    hor_position = ord(guess[0]) - (ord("A"))
+    ver_position = int(guess[1:]) - 1
+    
+    is_guess_within_bounds = hor_position < len(hidden_board) and ver_position <= len(hidden_board[0]) - 1 and ver_position != -1
+    if not is_guess_within_bounds:
+        print("Please use one letter, one number, within the gameboard's range.")
+        continue
+    
+    guess_pos = hidden_board[hor_position][ver_position]
+    visible_guess_pos = visible_board[hor_position][ver_position]
+    
+    is_it_previous_guess = visible_guess_pos == "H" or visible_guess_pos == "O"
+    is_ship_hit = guess_pos == "S"
+    if is_it_previous_guess:
+        torpedos -= 1
+        print(f"Hey, you've already tried that! You can only miss {torpedos} more times!")
+        print_board(visible_board)
+    elif is_ship_hit:
+        force -= 1
+        visible_board[hor_position][ver_position] = "H"
+        print(f"That's a hit!!! The enemy is left at {force} strength!")
+        print_board(visible_board)
     else:
-        hor_position = ord(guess[0]) - (ord("A"))
-        ver_position = int(guess[1:]) - 1
-        Is_Guess_Within_Bounds = hor_position >= len(hidden_board) or ver_position > len(hidden_board[0]) - 1
-        if Is_Guess_Within_Bounds:
-            print("Please use one letter, one number, within the gameboard's range.")
-        else:
-            guess_pos = hidden_board[hor_position][ver_position]
-            visible_guess_pos = visible_board[hor_position][ver_position]
-            Is_It_Previous_Guess = visible_guess_pos == "H" or visible_guess_pos == "O"
-            Is_Ship_Hit = guess_pos == "S"
-            if Is_It_Previous_Guess:
-                torpedos -= 1
-                print(f"Hey, you've already tried that! You can only miss {torpedos} more times!")
-                Print_Board(visible_board)
-            elif Is_Ship_Hit:
-                force -= 1
-                visible_board[hor_position][ver_position] = "H"
-                print(f"That's a hit!!! The enemy is left at {force} strength!")
-                Print_Board(visible_board)
-            else:
-                torpedos -= 1
-                visible_board[hor_position][ver_position] = "O"
-                print(f"That's a miss! You can only try {torpedos} more times!")
-                Print_Board(visible_board)
+        torpedos -= 1
+        visible_board[hor_position][ver_position] = "O"
+        print(f"That's a miss! You can only try {torpedos} more times!")
+        print_board(visible_board)
+        
 if torpedos == 0:
     print("Bad luck, Captain! We've lost!!!")
 if force == 0:
--- HangmanV0.0.py
+++ HangmanV0.0.py
@@ -1,57 +1,78 @@
 import random
 
 
-def Random_Item_From_Collection(collection):
+class Flavor_Text:
+    def __init__(self, collection):
+        self.options = list(collection)
+
+    def __str__(self):
+        return f"Here is a list with the options for the {self} flavor text: {self.options}."
+    
+    
+# randomizálás method?
+
+def random_item_from_collection(collection):
     return collection[random.randrange(len(collection))]
 
 
+fail_insults = Flavor_Text(["Wroooong! You suck!", "Wow, just, wow...", "Andrew, is that you?", "Hint: the word is in English. Do you speak it??? DOES HE LOOK LIKE A BI... sorry, sorry..."])
+success_insults = Flavor_Text(["GG", "You got one! Damn you!!!", "Even a broken clock...", "Well, color me impressed...", "OK, OK, but don't get cocky on me now."])
+final_insults = Flavor_Text(["Well, it seems you failed. Should I have made a tutorial or something?", "Oooooh, I'm so sorry, I didn't know you were illiterate!", "Wow... words fail me... as they have obviously failed you.", "I mean... should I bother insulting you, can you even read this?", "We're done here, STOP STARING AT ME!!!"])
+guess_prompts = Flavor_Text(["Make your guess, foolish mortal: ", "Come on, try something: ", "Gimme a letter: ", "Just press one of the buttons in front of you, meatbag: ", "Please insert letter: "])
+print(guess_prompts)
+
 words = ["tequila", "sunset", "disco", "dancer", "column", "mariner", "lighthouse", "insanity", "janitor", "rat", "cheese", "peach", "laxative", "archer", "tactical", "turtleneck", "oedipus", "literary", "difficulty", "msspelled", "xenophon"]
-welcome = ["Hey there! Welcome!", "How you doin'?", "What are you looking at, weirdo? Oh yeah, the game...", "So glad to see you here!"]
-fail_insults = ["Wroooong! You suck!", "Wow, just, wow...", "Andrew, is that you?", "Hint: the word is in English. Do you speak it??? DOES HE LOOK LIKE A BI... sorry, sorry..."]
-success_insults = ["GG", "You got one! Damn you!!!", "Even a broken clock...", "Well, color me impressed...", "OK, OK, but don't get cocky on me now."]
-final_insults = ["Well, it seems you failed. Should I have made a tutorial or something?", "Oooooh, I'm so sorry, I didn't know you were illiterate!", "Wow... words fail me... as they have obviously failed you.", "I mean... should I bother insulting you, can you even read this?", "We're done here, STOP STARING AT ME!!!"]
-guess_prompts = ["Make your guess, foolish mortal: ", "Come on, try something: ", "Gimme a letter: ", "Just press one of the buttons in front of you, meatbag: ", "Please insert letter: "]
-previous_guess = []
-selected_word = Random_Item_From_Collection(words)
-representation = "X"*len(selected_word)
-print(Random_Item_From_Collection(welcome))
-print(representation)
+# welcome = ["Hey there! Welcome!", "How you doin'?", "What are you looking at, weirdo? Oh yeah, the game...", "So glad to see you here!"]
+# fail_insults = ["Wroooong! You suck!", "Wow, just, wow...", "Andrew, is that you?", "Hint: the word is in English. Do you speak it??? DOES HE LOOK LIKE A BI... sorry, sorry..."]
+# success_insults = ["GG", "You got one! Damn you!!!", "Even a broken clock...", "Well, color me impressed...", "OK, OK, but don't get cocky on me now."]
+# final_insults = ["Well, it seems you failed. Should I have made a tutorial or something?", "Oooooh, I'm so sorry, I didn't know you were illiterate!", "Wow... words fail me... as they have obviously failed you.", "I mean... should I bother insulting you, can you even read this?", "We're done here, STOP STARING AT ME!!!"]
+# guess_prompts = ["Make your guess, foolish mortal: ", "Come on, try something: ", "Gimme a letter: ", "Just press one of the buttons in front of you, meatbag: ", "Please insert letter: "]
+# previous_guess = []
+# selected_word = random_item_from_collection(words)
+# representation = "X" * len(selected_word)
+# tries = 8
 
 
+# print(random_item_from_collection(welcome))
+# print(representation)
 
-tries = 8
-while tries > 0:
-    guess = input(Random_Item_From_Collection(guess_prompts))
-    guess = guess.lower()
-    Already_Tried_That = guess in previous_guess
-    if Already_Tried_That:
-        tries -= 1
-        print(f"Come on, mate, you've already tried that! There goes one of your guesses, you've got {tries} remaining...")
-    else:
-        previous_guess.append(guess)
-        Not_Single_Letter_Nor_Solve = guess != selected_word and len(guess) != 1
-        Not_A_Letter = not guess.isalpha()
-        Full_Correct_Guess = guess == selected_word
-        Wrong_Guess = guess not in selected_word
-        if Not_Single_Letter_Nor_Solve:
-            tries -= 1
-            print(f"Either guess the full word correctly, or take it one letter at a time, buddy. You have {tries} guesses remaining!")  
-        elif Not_A_Letter:
-            tries -= 1
-            print(f"Come on, buddy... just what are you trying to pull? You have {tries} guesses remaining!")
-        elif Full_Correct_Guess:
-            print("You got it all in one! How could you do this to me???")
-            break
-        elif Wrong_Guess:
-            tries -= 1
-            print(Random_Item_From_Collection(fail_insults), f"You have {tries} guesses remaining!")
-        elif guess in selected_word:
-            for i, word in enumerate(selected_word):
-                if selected_word[i] == guess:
-                    representation = representation[:i] + guess + representation[i+1:]
-            if representation == selected_word:
-                print("I guess we're done here...")
-                break
-            print(Random_Item_From_Collection(success_insults), representation)
-if tries == 0:
-    print(Random_Item_From_Collection(final_insults), f"The word was {selected_word}!")
+# while tries > 0:
+    
+#     guess = input(random_item_from_collection(guess_prompts))
+#     guess = guess.lower()
+    
+#     already_tried_that = guess in previous_guess
+#     if already_tried_that:
+#         tries -= 1
+#         print(f"Come on, mate, you've already tried that! There goes one of your guesses, you've got {tries} remaining...")
+#         continue
+    
+#     previous_guess.append(guess)
+#     not_single_letter_nor_solve = guess != selected_word and len(guess) != 1
+#     not_letter = not guess.isalpha()
+#     full_correct_guess = guess == selected_word
+#     wrong_guess = guess not in selected_word
+#     if not_single_letter_nor_solve:
+#         tries -= 1
+#         print(f"Either guess the full word correctly, or take it one letter at a time, buddy. You have {tries} guesses remaining!")  
+#     elif not_letter:
+#         tries -= 1
+#         print(f"Come on, buddy... just what are you trying to pull? You have {tries} guesses remaining!")
+#     elif full_correct_guess:
+#         print("You got it all in one! How could you do this to me???")
+#         break
+#     elif wrong_guess:
+#         tries -= 1
+#         print(random_item_from_collection(fail_insults), f"You have {tries} guesses remaining!")
+
+#     elif guess in selected_word:
+#         for i, word in enumerate(selected_word):
+#             if selected_word[i] == guess:
+#                 representation = representation[:i] + guess + representation[i+1:]
+#         if representation == selected_word:
+#             print("I guess we're done here...")
+#             break
+#         print(random_item_from_collection(success_insults), representation)
+        
+# if tries == 0:
+#     print(random_item_from_collection(final_insults), f"The word was {selected_word}!")
